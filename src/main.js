41ee8ca9ec93830888922c29854ad9b68d55fef5
Your Name <santino.sablin@mail.ru>
2022-10-04 16:12:28 +0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/scss/main.scss';
+import './assets/utils/reset.scss';
+import './assets/scss/common/_common.scss';
+
+createApp(App).mount('#app')
